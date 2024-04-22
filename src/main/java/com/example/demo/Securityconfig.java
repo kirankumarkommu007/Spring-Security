14047dfa27d0df0fa06a719755063cfa3ffe577f
kirankumarkommu007 <kommukirankumar007@gmail.com>
2024-04-22 22:49:22 +0530

    added inmemory details

--- src/main/java/com/example/demo/Securityconfig.java
+++ src/main/java/com/example/demo/Securityconfig.java
@@ -4,15 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     
 	
-	//security with custom userdetails and password in application properties
+	//security with custom userdetails and password inline memory storage
 	
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -30,5 +34,17 @@ public class SecurityConfig {
         return http.build();
     }
     
+    @Bean 
+    public UserDetailsService userDetailsService() {
+    	UserDetails user=User.builder()
+    			.username("kiran")
+                .password(passwordEncoder().encode("1234"))
+    			.build();
+		return new InMemoryUserDetailsManager(user);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
    
 }
