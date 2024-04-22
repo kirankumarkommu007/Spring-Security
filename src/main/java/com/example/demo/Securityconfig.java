22c776e2db81dae8b73d315cae115e973ea00016
kirankumarkommu007 <kommukirankumar007@gmail.com>
2024-04-22 22:38:48 +0530

    added custom deatils in properties

--- src/main/java/com/example/demo/Securityconfig.java
+++ src/main/java/com/example/demo/Securityconfig.java
@@ -4,13 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     
 	
-	//security with custom login page
+	//security with custom userdetails and password in application properties
+	
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authorizeRequests ->
@@ -26,4 +29,6 @@ public class SecurityConfig {
 
         return http.build();
     }
+    
+   
 }
