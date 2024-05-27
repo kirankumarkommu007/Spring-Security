a364c666ef0757f3e4273a93c954cbe3d57990e7
kirankumarkommu007 <kommukirankumar007@gmail.com>
2024-05-27 13:21:43 +0530

    RBAC_AUTH0

---
+++ src/main/java/com/example/demo/RbacAuth0Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacAuth0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbacAuth0Application.class, args);
+	}
+
+}
